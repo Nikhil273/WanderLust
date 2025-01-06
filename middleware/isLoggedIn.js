ffc3865350971d2c8f64a4ad4683513caeb6b9da
Nikhil273 <nikhilmaurya.it@gmail.com>
2025-01-06 16:09:33 +0530

    Adding post login function

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,8 +1,18 @@
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
-    req.session.returnTo = req.originalUrl;
+    // post the current url so that we can redirect back to the page after login
+    req.session.redirectUrl = req.originalUrl;
     req.flash("error", "You must be Login in first!");
     return res.redirect("/login");
   }
+
+  next();
+};
+
+module.exports.saveRedirectUrl = (req, res, next) => {
+  if (req.session.redirectUrl) {
+    res.locals.returnTo = req.session.redirectUrl;
+  }
+
   next();
 };
