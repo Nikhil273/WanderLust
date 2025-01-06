1fa66f2a662b0b9922647388eef73e893b927858
Nikhil273 <nikhilmaurya.it@gmail.com>
2025-01-06 15:15:01 +0530

    Define local variables

--- app.js
+++ app.js
@@ -56,6 +56,7 @@ passport.deserializeUser(User.deserializeUser()); //deserialize user
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
+  res.locals.currentUser = req.user;
   next();
 });
 
